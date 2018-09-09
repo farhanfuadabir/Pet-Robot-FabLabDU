2ee533330c2cb449c9a4d07088e8ff870d5188a1
Atick Faisal <atick.faisal@ieee.org>
2018-09-09 21:10:44 +0600

    added blink.py

--- src/gpio_test.py
+++ src/gpio_test.py
@@ -3,12 +3,12 @@ import time as tm
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(39, GPIO.OUT)
+GPIO.setup(37, GPIO.OUT)
 
 while(True):
-     GPIO.output(39, True)
+     GPIO.output(37, True)
      tm.sleep(1)
-     GPIO.output(19, False)
+     GPIO.output(37, False)
      tm.sleep(1)
 
  GPIO.cleanup()
